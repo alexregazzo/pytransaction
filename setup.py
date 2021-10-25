cfc729ae1580cc7dbfd045cc4c275c1ef396bf62
alexregazzo <alex_regazzo@hotmail.com>
2021-10-24 22:21:51 -0300

    v0.1.0

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='pytransaction',
+    version='0.1.0',
+    description='Python library for class transaction',
+    author='Alex Regazzo',
+    author_email='alex_regazzo@hotmail.com',
+    url='https://github.com/alexregazzo/pytransaction/',
+    packages=["pytransaction"],
+    install_requires=[],
+)
